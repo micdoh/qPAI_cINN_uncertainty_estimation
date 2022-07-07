dc4d908cac7d00bd15f420dc9fc5feda43f2499f
mic.d.doh@gmail.com <17Blackmamba>
2022-07-07 13:51:08 +0100

    Minimal example condition joining successfully

---
+++ qPAI_cINN_uncertainty_estimation/data.py
@@ -0,0 +1,30 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+def switch_seq_feat(tensor):
+    # Return a view of the tesor with axes rearranged
+    return torch.permute(tensor, (1, 0)).float()
+
+def float_transform(tensor):
+    return tensor.float()
+
+class MultiSpectralPressureO2Dataset(Dataset):
+    def __init__(self, spectra, oxygenations):
+        self.data = spectra
+        self.labels = oxygenations
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        data = self.data[idx]
+        label = self.labels[idx]
+        if self.transform:
+            data = self.transform(data)
+        if self.target_transform:
+            label = self.target_transform(label)
+        # TODO - How can I add the input Gaussian to the dataloader? What does the input Gaussian look like? Would it be easier/preferable to input the outputs? Also, probably the Gaussian should be the same every time (?), such that the only information introduced is from the conditioning
+        # TODO - Alternativel, can have the Gaussian within the Wrapped Model class and it gets entered on the forward pass.
+        return data, label
---
+++ qPAI_cINN_uncertainty_estimation/normalisation.py
@@ -0,0 +1,39 @@
+import torch
+import random
+import numpy as np
+
+def spectrum_normalisation(spectrum):
+    """Applies z-score scaling to the initial pressure spectrum"""
+    mean = np.mean(spectrum)
+    std = np.std(spectrum)
+    norm = (spectrum - mean)/std
+    return norm
+
+def spectrum_processing(spectrum, allowed_datapoints):
+    """Returns a normalised initial pressure spectrum with some of the values zeroed out"""
+    num_non_zero_datapoints = random.choice(allowed_datapoints)
+    a = np.zeros(len(spectrum))
+    a[:num_non_zero_datapoints] = 1
+    np.random.shuffle(a)
+
+    incomplete_spectrum = list(np.multiply(a, np.array(spectrum)))
+    non_zero_indices = np.nonzero(incomplete_spectrum)
+    non_zero_values = list(filter(None,incomplete_spectrum))
+    normalised_non_zero = spectrum_normalisation(non_zero_values)
+
+    i = 0
+    for index in non_zero_indices[0]:
+        incomplete_spectrum[index] = normalised_non_zero[i]
+        i+=1
+
+    normalised_incomplete_spectrum = np.array(incomplete_spectrum)
+
+    return normalised_incomplete_spectrum
+
+def batch_spectrum_processing(batch, allowed_datapoints):
+    processed = []
+
+    for spectrum in batch:
+
+        processed.append(spectrum_processing(spectrum, allowed_datapoints))
+    return torch.tensor(np.array(processed))
