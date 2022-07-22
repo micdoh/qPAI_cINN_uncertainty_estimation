06b47b45b01c020b579c8b1eb30e04ed19c5566a
mic.d.doh@gmail.com <17Blackmamba>
2022-07-22 15:50:17 +0100

    Save calibration dataframe

--- qPAI_cINN_uncertainty_estimation/config.py
+++ qPAI_cINN_uncertainty_estimation/config.py
@@ -45,7 +45,8 @@ sample_posterior = True  # Draw n_samples to find posterior, otherwise just samp
 visualisation = False if use_cuda else True
 load_eval_data = False if use_cuda else True
 save_eval_data = True if use_cuda else False
-load_eval_data_date = '2022-07-15_00_16_03'
+#load_eval_data_date = '2022-07-15_00_16_03'
+load_eval_data_date = "2022-07-21_19_03_25"  # Wide model, less blocks, with FCN
 
 #######################
 #  Training schedule  #
--- qPAI_cINN_uncertainty_estimation/eval.py
+++ qPAI_cINN_uncertainty_estimation/eval.py
@@ -112,6 +112,11 @@ def calibration_error(test_loader, dir=None):
         plt.savefig(file.resolve())
 
     plt.show()
+    calib_df = pd.DataFrame({"confidence": confidences,
+                             "uncert_interval": uncert_intervals,
+                             "inliers": inliers,
+                             "calib_err": calib_err,
+                             })
 
     # Load everything into a dataframe
     df = pd.DataFrame({"mean_pred": mean_pred,
@@ -127,7 +132,7 @@ def calibration_error(test_loader, dir=None):
                        })
     df.sort_values(by=["g_truth"], ascending=False, inplace=True)
 
-    return df
+    return df, calib_df
 
 
 if __name__ == "__main__":
@@ -174,42 +179,43 @@ if __name__ == "__main__":
 
     if c.load_eval_data:
         df_file = c.output_dir / c.load_eval_data_date / f"{c.load_eval_data_date}@dataframe.csv"
+        calib_df_file = c.output_dir / c.load_eval_data_date / f"{c.load_eval_data_date}@calib_dataframe.csv"
         df = pd.read_csv(df_file.resolve())
+        calib_df = pd.read_csv(calib_df_file.resolve())
 
     else:
 
-        labels = np.array([])
-        preds = np.array([])
-        rel_err = np.array([])
-        abs_err = np.array([])
-        iqr_upper = np.array([])
-        iqr_lower = np.array([])
+        model.eval()
 
-        model.eval()  # Optional when not using Model Specific layer
-
-        df = calibration_error(test_dataloader, dir=output_dir)
+        df, calib_df = calibration_error(test_dataloader, dir=output_dir)
 
         if c.save_eval_data:
             output_dir.mkdir(parents=True, exist_ok=True)
             df_file = output_dir / f"{start_time}@dataframe.csv"
+            calib_df_file = output_dir / f"{start_time}@calib_dataframe.csv"
             df.to_csv(df_file.resolve())
+            calib_df.to_csv(calib_df_file.resolve())
             config_details_file = output_dir / f"{start_time}@test_config.txt"
             logger.info(config_string(config_details_file.resolve()))
             logger.info(f"Data saved to: {df_file.resolve()}")
+            logger.info(f"Calibration data saved to: {calib_df_file.resolve()}")
 
     if c.visualisation:
 
-        fig, ax = plt.subplots()
-        fig.subplots_adjust(top=0.8)
-        ax.set_xlim(0, len(df))
-        ax.set_ylim(0, 1)
-        ax.set_ylabel("sO2")
-        ax.set_xlabel("Sample number")
-        x = np.arange(0, len(df))
-        y = df["preds"]
-        ax.errorbar(
+        def get_relative_iqrs(name):
+            return np.vstack((df["iqr_upper"]-df[name], df[name]-df["iqr_lower"]))
+
+        mean_fig, mean_ax = plt.subplots()
+        mean_fig.subplots_adjust(top=0.8)
+        mean_ax.set_xlim(0, 100)
+        mean_ax.set_ylim(0, 100)
+        mean_ax.set_ylabel("Mean predicted sO2 (%)")
+        mean_ax.set_xlabel("Ground truth sO2 (%)")
+        x = df["g_truth"]*100
+        y = df["mean_pred"]*100
+        mean_ax.errorbar(
             x, y,
-            yerr=df["stdevs"],
+            yerr=get_relative_iqrs('mean_pred')*100,
             elinewidth=0.1,
             capsize=1,
             ecolor='r',
@@ -219,27 +225,62 @@ if __name__ == "__main__":
             ms=1,
             fmt='o'
         )
-        ax.plot(x, df["labels"])
-        pm = '$\pm$1'
-        sampling_type = f"{'random' if c.sample_posterior else pm} sampling"
-        ax.set_title(f"sO2 predictions of cINN based on \n"
-                  f"{sampling_type} from Gaussian latent space")
-        fig.show()
+        mean_ax.set_title(f"Mean sO2 predictions of cINN based on 1000 samples")
+        mean_ax.plot([0, 0], [100, 100], color='k', linestyle='-', linewidth=2, zorder=100)
+        mean_fig.show()
 
-        err_fig, err_ax = plt.subplots()
-        err_ax.set_xlim(0, 1)
-        err_ax.set_title('Absolute prediction error for sO2 values\n'
-                         f'{sampling_type}')
-        err_ax.set_xlabel('sO2')
-        err_ax.set_ylabel('Absolute error (prediction - ground truth)')
-        err_ax.scatter(np.arange(0, 1, 1/len(df)), df["errors"], s=1)
-        err_fig.show()
+        median_fig, median_ax = plt.subplots()
+        median_fig.subplots_adjust(top=0.8)
+        median_ax.set_xlim(0, 100)
+        median_ax.set_ylim(0, 100)
+        median_ax.set_ylabel("Median predicted sO2 (%)")
+        median_ax.set_xlabel("Ground truth sO2 (%)")
+        x = df["g_truth"]*100
+        y = df["median_pred"]*100
+        median_ax.errorbar(
+            x, y,
+            yerr=get_relative_iqrs('median_pred')*100,
+            elinewidth=0.1,
+            capsize=1,
+            ecolor='r',
+            errorevery=10,
+            markevery=10,
+            ls='none',
+            ms=1,
+            fmt='o',
+            zorder=2
+        )
+        median_ax.set_title(f"Median sO2 predictions of cINN based on 1000 samples")
+        median_ax.plot([0, 0], [100, 100], color='k', linestyle='-', linewidth=2, zorder=100)
+        median_fig.show()
+
+        abs_err_fig, abs_err_ax = plt.subplots()
+        abs_err_ax.set_xlim(0, 100)
+        abs_err_ax.set_title('Absolute prediction error for predicted sO2 values')
+        abs_err_ax.set_xlabel('Ground truth sO2 (%)')
+        abs_err_ax.set_ylabel('Absolute error (%)')
+        abs_err_ax.scatter(df["g_truth"]*100, df["abs_err_median"]*100, s=1, label="Median prediction")
+        #abs_err_ax.scatter(df["g_truth"]*100, df["abs_err_mean"]*100, s=1, label="Mean prediction")
+        #abs_err_ax.legend()
+        abs_err_fig.show()
 
         rel_err_fig, rel_err_ax = plt.subplots()
-        rel_err_ax.set_xlim(0, 1)
-        rel_err_ax.set_title('Relative prediction error for sO2 values\n'
-                             f'{sampling_type}')
-        rel_err_ax.set_xlabel('sO2')
-        rel_err_ax.set_ylabel('Relative error (Upper and lower stdevs)')
-        rel_err_ax.scatter(np.arange(0, 1, 1/len(df)), np.add(df["stdevs_lower"], df["stdevs_upper"]), s=1)
+        rel_err_ax.set_yscale('log')
+        rel_err_ax.set_xlim(0, 100)
+        rel_err_ax.set_title('Relative prediction error for predicted sO2 values')
+        rel_err_ax.set_xlabel('Ground truth sO2 (%)')
+        rel_err_ax.set_ylabel('Relative error (%)')
+        rel_err_ax.scatter(df["g_truth"]*100, df["rel_err_median"]*100, s=1, label="Median prediction")
+        #rel_err_ax.scatter(df["g_truth"]*100, df["rel_err_mean"]*100, s=1, label="Mean prediction")
+        #rel_err_ax.legend()
         rel_err_fig.show()
+        
+        pred_diff_fig, pred_diff_ax = plt.subplots()
+        pred_diff_ax.set_xlim(0, 100)
+        pred_diff_ax.set_title('Difference between Mean/Median')
+        pred_diff_ax.set_xlabel('Ground truth sO2 (%)')
+        pred_diff_ax.set_ylabel('Median - Mean (%)')
+        pred_diff_ax.scatter(df["g_truth"]*100, df["mean_pred"]*100-df["median_pred"]*100, s=1, label="Median prediction")
+        pred_diff_fig.show()
+
+
