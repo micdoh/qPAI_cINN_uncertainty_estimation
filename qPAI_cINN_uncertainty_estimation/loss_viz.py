39ff22047aadba14716fce0357e0ed41b25752a3
mic.d.doh@gmail.com <17Blackmamba>
2022-07-25 15:04:25 +0100

    Configurable start index for loss viz

--- qPAI_cINN_uncertainty_estimation/loss_viz.py
+++ qPAI_cINN_uncertainty_estimation/loss_viz.py
@@ -5,23 +5,29 @@ import numpy as np
 import qPAI_cINN_uncertainty_estimation.config as c
 
 
-def read_files_into_array(date: str, name: str, suffix: str = '.npy', seq: bool = True, axis: int = 1):
+def read_files_into_array(date: str,
+                          name: str,
+                          suffix: str = '.npy',
+                          seq: bool = True,
+                          axis: int = 1,
+                          start_index: int = 1
+                          ):
     """Read files into a numpy array
     If seq = True, read a series of files and add an index"""
     dir = c.output_dir / date
-    index = 0
+    index = start_index
     if seq:
         while True:
             try:
                 file = dir / f"{date}@{name}_{index}{suffix}"
                 with open(file.resolve(), 'rb') as f:
                     data = np.load(f)
-                    if index > 0:
+                    if index > start_index:
                         data = np.concatenate((old_data, data), axis=axis)
                     old_data = data
                     index += 1
             except FileNotFoundError:
-                print('filenotound')
+                print(f'File not found: {file.resolve()}')
                 break
     else:
         file = dir / f"{date}@{name}{suffix}"
@@ -30,28 +36,23 @@ def read_files_into_array(date: str, name: str, suffix: str = '.npy', seq: bool 
     return data
 
 
-#def viz_epoch_loss(fig, date: str):
-
-
-#def viz_
-
-
 if __name__ == "__main__":
     #date = '2022-07-12_11_08_52'  # Initial model
     #date = '2022-07-14_17_52_20'  # Larger model
-    #date = '2022-07-20_17_16_01'  # Wider model, less blocks, no FCN
-    date = '2022-07-21_09_39_35'  # Wider model, less blocks, with FCN
+    #date = '2022-07-20_17_16_01'  # Wider model, less blocks, no FCN, 10 wavelengths
+    #date = '2022-07-21_09_39_35'  # Wider model, less blocks, with FCN, 10 wavelengths
+    date = '2022-07-24_21_29_34'  # Wider model, less blocks, with FCN, 40 wavelengths
 
     loss_data = read_files_into_array(date, 'loss_epoch', axis=1)
     fig, ax = plt.subplots()
-    ax.set_title('Batch Losses')
+    ax.set_title('Training Loss - Batches')
     ax.set_xlabel('Batches')
     ax.set_ylabel('NLL Loss')
     ax.plot(np.arange(loss_data.shape[1]), loss_data.T)
 
     loss_data = read_files_into_array(date, 'loss_epoch', axis=0)
     fig1, ax1 = plt.subplots()
-    ax1.set_title('Batch Losses')
+    ax1.set_title('Training Loss - Batches')
     ax1.set_xlabel('Batches')
     ax1.set_ylabel('NLL Loss')
     for i in range(len(loss_data)):
@@ -61,7 +62,7 @@ if __name__ == "__main__":
 
     loss_data = read_files_into_array(date, 'epoch_losses', seq=False, axis=0)
     fig2, ax2 = plt.subplots()
-    ax2.set_title('Epoch Losses')
+    ax2.set_title('Training Loss - Epochs')
     ax2.set_xlabel('Epochs')
     ax2.set_ylabel('NLL Loss')
     ax2.plot(np.arange(loss_data.shape[1]), loss_data.T)
