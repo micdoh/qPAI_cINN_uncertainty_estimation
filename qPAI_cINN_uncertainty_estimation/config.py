1b4a5455853404e82558c75b434d0aa2c5ef856d
mic.d.doh@gmail.com <17Blackmamba>
2022-08-10 15:27:59 +0100

    cuda:1 and no partitioning

--- qPAI_cINN_uncertainty_estimation/config.py
+++ qPAI_cINN_uncertainty_estimation/config.py
@@ -8,7 +8,7 @@ Originally from: https://github.com/VLL-HD/analyzing_inverse_problems/
 """
 
 use_cuda = torch.cuda.is_available()
-device = torch.device("cuda:0" if use_cuda else "cpu")
+device = torch.device("cuda:1" if use_cuda else "cpu")
 torch.backends.cudnn.benchmark = True
 
 if use_cuda:
@@ -38,7 +38,7 @@ cond_length = seq_length * lstm_hidden if not use_fcn_layer else seq_length*fcn_
 
 experiment_name = ""
 allowed_datapoints = [40]
-partition_sparsity = True  # Partition spectra by sparsity with equal number of wavelengths in each partition
+partition_sparsity = False  # Partition spectra by sparsity with equal number of wavelengths in each partition
 
 n_samples = 1000  # Number of samples for inference
 
